716f7d765c0808bff740f47a22e1a59a21e0a7b8
Nikolay Amiantov <ab@fmap.me>
2016-11-28 14:20:47 +0300

    Use dot instead of underscore in unstable version
    On Debian underscores in versions are prohibited.

--- release.nix
+++ release.nix
@@ -19,7 +19,7 @@ let
       releaseTools.sourceTarball {
         name = "nix-tarball";
         version = builtins.readFile ./version;
-        versionSuffix = if officialRelease then "" else "pre${toString nix.revCount}_${nix.shortRev}";
+        versionSuffix = if officialRelease then "" else "pre${toString nix.revCount}.${nix.shortRev}";
         src = if lib.inNixShell then null else nix;
         inherit officialRelease;
 
