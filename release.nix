434d7c7519f1aff28cf6642d086511ecc6dc31ea
Nikolay Amiantov <ab@fmap.me>
2016-11-28 14:15:11 +0300

    Use OpenSSL-flavored libcurl on Debian
    It's installed by default; also, NSS flavour doesn't support certificate
    verification by default.

--- release.nix
+++ release.nix
@@ -324,7 +324,7 @@ let
       src = jobs.tarball;
       diskImage = (diskImageFun vmTools.diskImageFuns)
         { extraPackages =
-            [ "libdbd-sqlite3-perl" "libsqlite3-dev" "libbz2-dev" "libwww-curl-perl" "libcurl-dev" "libcurl3-nss" "libssl-dev" "liblzma-dev" ]
+            [ "libdbd-sqlite3-perl" "libsqlite3-dev" "libbz2-dev" "libwww-curl-perl" "libcurl-dev" "libcurl4-openssl-dev" "libssl-dev" "liblzma-dev" ]
             ++ extraPackages; };
       memSize = 1024;
       meta.schedulingPriority = 50;
